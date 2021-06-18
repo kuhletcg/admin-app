7cc76dc900e640c0363642bb29c6f43408148bae
TCG <ssibonokuhle@gmail.com>
2021-06-18 08:41:33 +0200

    Add server file where my port is running

---
+++ admin-app/backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { employeeRoutes } = require("./routes/api/employee");
+
+app.use(cors());
+require("dotenv/config");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+employeeRoutes(app);
+app.listen(5050, () => console.log(`Server is running on port 5000...`));
