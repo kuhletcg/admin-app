7928c0d8285ffa5357bcdfdb431e0c11180b7e58
TCG <ssibonokuhle@gmail.com>
2021-06-23 10:18:09 +0200

    Imported addEmployee from redux/actions/employee

--- admin-app/frontend/src/components/useForm.js
+++ admin-app/frontend/src/components/useForm.js
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addEmployee } from "../redux/actions/employee";
+
 const useForm = () => {
   const [formInfo, setFormInfo] = useState({
     name: "",
@@ -8,13 +11,22 @@ const useForm = () => {
     salary: "",
   });
   const [errors, setErrors] = useState({});
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormInfo({ ...formInfo, [name]: value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    setErrors(validate(formInfo));
   };
+
+  useEffect(() => {
+    if (Object.keys(error).length === 0) {
+      dispatch(addEmployee(formInfo));
+    }
+  }, [error]);
   return { formInfo, errors, handleChange, handleSubmit };
 };
 
